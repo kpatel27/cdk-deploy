fe6283095a5685c6ca0431b6af93b900454f83a5
Krunal Patel <kppatel727@gmail.com>
2020-07-20 14:47:16 -0700

    initial setup

--- bin/cdk-deploy.js
+++ bin/cdk-deploy.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
-const cdk = require('@aws-cdk/core');
-const { CdkDeployStack } = require('../lib/cdk-deploy-stack');
+const cdk = require("@aws-cdk/core");
+const { CdkDeployStack } = require("../lib/cdk-deploy-stack");
 
 const app = new cdk.App();
-new CdkDeployStack(app, 'CdkDeployStack');
+new CdkDeployStack(app, "CdkDeployStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
